aa9a7239975ac233efc701c9ebcef23bae55ff93
hannahashe <hannahashe13@gmail.com>
2026-02-05 02:21:47 +0000

    Add updated_on field to Post model for tracking modifications

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post # importing from a file named models in the same directory as the admin.py file
+# seperate multiple models for import with a comma
 
 # Register your models here.
 admin.site.register(Post)
