4911559c56c37401366f4e82867a5e30a241e71f
hannahashe <hannahashe13@gmail.com>
2026-02-05 14:43:32 +0000

    Enhance Post and Comment models with ordering and string representation methods

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Post, Comment  #  importing from a file named models in the same directory as the admin.py file, seperate multiple models for import with a comma
+from .models import Post, Comment
+
+# importing from a file named models in the same directory
+# as the admin.py file,
+# separate multiple models for import with a comma
 
 # Register your models here.
 admin.site.register(Post)
