fdbafb8ee344fc1db98fe05f14cd2a167866f4c9
hannahashe <hannahashe13@gmail.com>
2026-02-05 14:25:27 +0000

    Add Comment model and migration for enhanced post interaction

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Post # importing from a file named models in the same directory as the admin.py file
-# seperate multiple models for import with a comma
+from .models import Post, Comment  #  importing from a file named models in the same directory as the admin.py file, seperate multiple models for import with a comma
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Comment)
