23e394cc86b5f57ebe327f91b09e9ca09742e979
hannahashe <hannahashe13@gmail.com>
2026-02-06 14:40:00 +0000

    Register Post model with Summernote admin and update settings and URLs for django_summernote integration

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 # importing from a file named models in the same directory
 # as the admin.py file,
 # separate multiple models for import with a comma
 
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 # Register your models here.
-admin.site.register(Post)
+
+
 admin.site.register(Comment)
