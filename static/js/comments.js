761e9fecab8b4585ca48c9a0c06a3eedb8d132fe
hannahashe <hannahashe13@gmail.com>
2026-02-09 21:50:15 +0000

    Add delete comment functionality with confirmation modal and JavaScript support

--- static/js/comments.js
+++ static/js/comments.js
@@ -3,6 +3,10 @@ const commentText = document.getElementById("id_body");
 const commentForm = document.getElementById("comment-form");
 const submitButton = document.getElementById("submit-button");
 
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const deleteButtons = document.getElementsByClassName("btn-delete");
+const deleteConfirm = document.getElementById("deleteConfirm");
+
 for (let button of editButtons) {
     button.addEventListener("click", (e) => {
         let commentId = e.target.getAttribute("comment_id");
@@ -13,3 +17,25 @@ for (let button of editButtons) {
     });
 }
 
+/**
+ * Initializes delete functionality for the provided delete buttons.
+ * 
+ * For each button in the `deleteButtons` collection:
+ * - retrieves the associated comment's ID upon click.
+ * - Updates the `deleteConfirm` link's href to point to the
+ * deletion endpoint for the specific comment.
+ * - Displays a confirmation modal (`deleteModal`) to prompt 
+ * the user for confirmation before deletion.
+ * 
+ * This ensures that users have a chance to confirm their intent
+ * to delete a comment, preventing accidental deletions.
+ */
+
+
+for (let button of deleteButtons) {
+    button.addEventListener("click", (e) => {
+        let commentId = e.target.getAttribute("comment_id");
+        deleteConfirm.setAttribute("href", `delete_comment/${commentId}`);
+        deleteModal.show();
+    });
+}
